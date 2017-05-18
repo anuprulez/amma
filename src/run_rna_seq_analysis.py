0431d2771150e78652907c5e6e71f286eaaeef2c
Bérénice Batut <bebatut@gmail.com>
2017-05-18 16:54:26 +0200

    Add rules to merge fastq files and one to launch FastQC

---
+++ src/run_rna_seq_analysis.py
@@ -0,0 +1,117 @@
+from bioblend.galaxy import GalaxyInstance
+import pandas as pd
+
+
+configfile: "config.yaml"
+
+
+def check_hist(hist_name):
+    '''
+    Check if an history exist and return its id if it's exist
+    '''
+    hist = ''
+    for history in histories:
+        if history["name"] == hist_name:
+            hist = history['id']
+    return hist
+
+
+def get_tool_id(tool_name):
+    '''
+    Retrieve the id of a tool
+    '''
+    tool_id = ''
+    for tool in tools:
+        if tool["name"] == tool_name:
+            tool_id = tool["id"]
+
+
+# Connect to Galaxy and retrieve the history
+gi = GalaxyInstance(config["galaxy_url"], config["api_key"])
+histories = gi.histories.get_histories()
+hist = check_hist(config["hist_name"]["raw_data"])
+
+# Extract the sample names
+finename_desc_df = pd.read_csv("data/file_description.csv", index_col = 0)
+sample_names = list(finename_desc_df.index)
+
+# Get tools in the Galaxy instance
+tools = gi.tools.get_tools()
+
+
+rule merge_files:
+    '''
+    Merge files sequenced on 2 different lanes (for Project_S178 and Project_S225)
+    '''
+    run:
+        # Retrieve the name of samples to merge
+        to_merge = {}
+        for sample in sample_names:
+            project_id = finename_desc_df["Project id"][sample]
+            if project_id not in ["Project_S178", "Project_S225"]:
+                continue
+            to_merge.setdefault(sample, [])
+        # Parse the dataset in history to extract the ids of dataset to merge
+        for dataset in gi.histories.show_matching_datasets(hist):
+            name = dataset['name']
+            if not name.endswith("fastq"):
+                continue
+            sample_name = os.path.splitext(name)[0][:-1]
+            if sample_name in to_merge:
+                to_merge[sample_name].append(dataset["id"])
+            else:
+                # Rename to remove the last letter
+                gi.histories.update_dataset(
+                    hist,
+                    dataset["id"],
+                    name="%s%s" % (config["raw_data_name_prefix"], sample_name))
+        # Get concatenate tool
+        tool_id = get_tool_id("Concatenate datasets")
+        # Merge datasets
+        for dataset in to_merge:
+            if len(to_merge[dataset]) != 2:
+                print("Issue with %s" %(dataset))
+                continue
+            # Create the input datamap
+            datamap = dict()
+            datamap["inputs"] = [
+                {'src':'hda', 'id': to_merge[dataset][0]},
+                {'src':'hda', 'id': to_merge[dataset][1]}]
+            # Run the tool
+            info = gi.tools.run_tool(hist, tool_id, datamap)
+            # Rename the datasets
+            gi.histories.update_dataset(
+                hist,
+                info['outputs'][0]['id'],
+                name="%s%s" % (config["raw_data_name_prefix"], dataset))
+
+
+rule launch_fastqc:
+    '''
+    Launch FastQC on raw data
+    '''
+    run:
+        # Get (or create) the history for FastQC outputs
+        fastqc_hist = check_hist(config["hist_name"]["fastqc"])
+        if fastqc_hist == '':
+            fastqc_hist = gi.histories.create_history(
+                config["hist_name"]["fastqc"])["id"]
+        # Get the id for FastQC tool
+        tool_id = get_tool_id("FastQC")
+        # Launch FastQC tool on the raw dataset
+        for dataset in gi.histories.show_matching_datasets(hist):
+            name = dataset['name']
+            if not name.startswith(config["raw_data_name_prefix"]):
+                continue
+            # Create the input datamap
+            datamap = dict()
+            datamap["input_file"] = {'src':'hda', 'id': dataset["id"]}
+            # Run the tool
+            info = gi.tools.run_tool(fastqc_hist, tool_id, datamap)
+            # Rename the datasets
+            gi.histories.update_dataset(
+                hist,
+                info['outputs'][0]['id'],
+                name="%s%s" % (config["raw_data_name_prefix"], dataset))
+
+
